e5c1d136bdd135f941b0f5ece998063908c7a7b0
DreamingLeper <almir.islamovic1234@gmail.com>
2024-04-22 06:19:15 +0200

    Slide show updated

--- slideshow_movement.js
+++ slideshow_movement.js
@@ -1,19 +1,30 @@
-// Get the slide wrapper element
-var slideWrapper = document.querySelector('.slide-wrapper');
+var controls = document.querySelectorAll('.controls');
+for(var i=0; i<controls.length; i++){
+	controls[i].style.display = 'inline-block';
+}
 
-// Add event listeners to the buttons
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+
+function goToSlide(n){
+	slides[currentSlide].className = 'slide';
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+// Assign each button to a specific slide
 document.getElementById('home_btn').addEventListener('click', function() {
-    slideWrapper.style.marginLeft = '0px';
+    goToSlide(0);
 });
 
 document.getElementById('contact_btn').addEventListener('click', function() {
-    slideWrapper.style.marginLeft = '-728px';
+    goToSlide(1);
 });
 
 document.getElementById('information_btn').addEventListener('click', function() {
-    slideWrapper.style.marginLeft = '-1456px';
+    goToSlide(2);
 });
 
 document.getElementById('about_btn').addEventListener('click', function() {
-    slideWrapper.style.marginLeft = '-2184px';
+    goToSlide(3);
 });
